0d026c539956d103c34ed5b3e78537d23bb5068f
Pragyansh Chaturvedi <pragyansh.chaturvedi@canonical.com>
2025-09-03 01:01:21 +0530

    Fix syntax - make more pythonic

--- examples/execve.py
+++ examples/execve.py
@@ -1,9 +1,11 @@
-from pythonbpf.decorators import tracepoint
+from pythonbpf.decorators import tracepoint, syscalls
 from ctypes import c_void_p, c_int32
 
-@tracepoint("syscalls:sys_enter_execve")
+
+@tracepoint(syscalls.sys_enter_execve)
 def trace_execve(ctx: c_void_p) -> c_int32:
-    print("execve called\n")
+    print("execve called")
     return c_int32(0)
+
 
 LICENSE = "GPL"
--- pythonbpf/decorators.py
+++ pythonbpf/decorators.py
@@ -1,3 +1,11 @@
+from types import SimpleNamespace
+
+syscalls = SimpleNamespace(
+    sys_enter_execve="syscalls:sys_enter_execve",
+    sys_exit_execve="syscalls:sys_exit_execve",
+)
+
+
 def tracepoint(name: str):
     def wrapper(fn):
         fn._section = f"tracepoint/{name}"
