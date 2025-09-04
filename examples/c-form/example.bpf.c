ba3030a69481e7ed78b8ec7ebedb694a5bd3b64a
varun-r-mallya <varunrmallya@gmail.com>
2025-09-04 10:33:43 +0530

    Update bpf examples and remove function pass code

--- examples/c-form/example.bpf.c
+++ examples/c-form/example.bpf.c
@@ -2,31 +2,16 @@
 #include <bpf/bpf_helpers.h>
 #include <stdint.h>
 
-int trace_testing(void *ctx)
-{
-    bpf_printk("THISISACONSTANT");
-    bpf_printk("THISISCONSTANT2");
-    uint64_t a = 69;
-    bpf_printk("%d", a);
-    return 0;
+void test_function() {
+    bpf_printk("test_function called");
 }
 
 SEC("tracepoint/syscalls/sys_enter_execve")
 int trace_execve(void *ctx)
 {
-    if(ctx){
-        trace_testing(ctx);
-    } else {
-        bpf_printk("THISISANOTHERCONSTANT");
-    }
-    bpf_trace_printk("execve called\n", 15);
-    return 0;
-}
-
-SEC("tracepoint/syscalls/sys_exit_execve")
-int trace_randomname_exit(void *ctx)
-{
-    bpf_trace_printk("execve called to exit\n", 15);
+    bpf_printk("execve called");
+    bpf_printk("execve2 called");
+    test_function();
     return 0;
 }
 
--- examples/execve.py
+++ examples/execve.py
@@ -1,18 +1,15 @@
 from pythonbpf.decorators import section
-# from pythonbpf.decorators import tracepoint, syscalls
 from ctypes import c_void_p, c_int32
 
 #This is a test function
 def test_function():
     print("test_function called")
 
-# @tracepoint(syscalls.sys_enter_execve)
 @section("tracepoint/syscalls/sys_enter_execve")
 def trace_execve(ctx: c_void_p) -> c_int32:
     print("execve called")
     print("execve2 called")
     test_function()
     return c_int32(0)
-
 
 LICENSE = "GPL"
