cc626c38f7de8f316f0b682387f58d8249dc537f
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2025-10-03 17:13:02 +0530

    Move binops1 to tests/passing

--- tests/passing_tests/binops.py
+++ tests/passing_tests/binops.py
@@ -5,7 +5,7 @@ from ctypes import c_void_p, c_int64
 @bpf
 @section("tracepoint/syscalls/sys_enter_sync")
 def sometag(ctx: c_void_p) -> c_int64:
-    a = 1 + 2 + 1
+    a = 1 + 2 + 1 + 12 + 13
     print(f"{a}")
     return c_int64(0)
 
--- tests/passing_tests/binops1.py
+++ tests/passing_tests/binops1.py
@@ -3,11 +3,12 @@ from ctypes import c_void_p, c_int64
 
 
 @bpf
-@section("sometag1")
+@section("tracepoint/syscalls/sys_enter_sync")
 def sometag(ctx: c_void_p) -> c_int64:
     b = 1 + 2
     a = 1 + b
-    return c_int64(a)
+    print(f"{a}")
+    return c_int64(0)
 
 
 @bpf
